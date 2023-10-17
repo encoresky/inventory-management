2a6e830ab53cdcc4b53677c8920fa6a465965614
rajat-est <rajat.saraswat@encoresky.com>
2023-10-17 13:00:31 +0530

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { dev }) => {
+        // Set devtool to 'false' to disable source maps in both development and production
+        config.devtool = false;
+
+        return config;
+    },
+}
 
 module.exports = nextConfig
